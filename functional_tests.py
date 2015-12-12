893e2eecc76cf2838a01ffed6638ead99b828548
Shannon Mulloy <smulloy21@gmail.com>
2015-12-11 23:04:13 -0800

    switch to unittest for fts

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,18 @@
+import unittest
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_starting_a_new_blog(self):
+        # Monty the Python loads up the page
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
