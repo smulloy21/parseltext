91128c10d21f754bf73752ba2a8e17723f11871a
Shannon Mulloy <smulloy21@gmail.com>
2015-12-13 23:51:42 -0800

    refactor home_page unit test

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from blogs.views import home_page
 
 # Create your tests here.
 class HomePageViewTest(TestCase):
 
-    def test_home_page_returns_html(self):
+    def test_home_page_uses_home_template(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>ParselText</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
+        expected = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected)
